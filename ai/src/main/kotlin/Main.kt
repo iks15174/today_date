cef78ff892aec611c14f32280156f940a037996c
ParkJiHo <iks15174@daum.net>
2024-12-26 14:58:11 +0900

    멀티모듈구성

---
+++ ai/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.pjh
+
+fun main() {
+    println("Hello World!")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+allprojects {
+	repositories {
+		mavenCentral()
+	}
+}
+
+subprojects {
+	group = "com.pjh"
+	version = "0.0.1-SNAPSHOT"
+
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+	apply(plugin = "kotlin")
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-web")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
+
+	allOpen {
+		annotation("javax.persistence.Entity")
+		annotation("javax.persistence.MappedSuperclass")
+		annotation("javax.persistence.Embeddable")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	kotlin {
+		compilerOptions {
+			freeCompilerArgs.addAll("-Xjsr305=strict")
+		}
+	}
+
+	java {
+		toolchain {
+			languageVersion = JavaLanguageVersion.of(21)
+		}
+	}
+}
